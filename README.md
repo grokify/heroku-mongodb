674b23e9da2473589f9285fd36d237c633e32783
John Wang <johncwang@gmail.com>
2023-12-17 03:18:29 -0800

    lint: golangci: fix various; update `README.md`

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Heroku MongoDB Free
 
 [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+Deploy a MongoDB instance on Heroku.
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -15,9 +16,25 @@ func main() {
 	}
 
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
+		if _, err := io.WriteString(w, "Hello, world!\n"); err != nil {
+			log.Print(err.Error())
+		}
 	}
 
-	http.HandleFunc("/", helloHandler)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	timeout := time.Second
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", helloHandler)
+
+	svr := &http.Server{
+		Addr:              ":" + port,
+		Handler:           mux,
+		IdleTimeout:       timeout,
+		ReadHeaderTimeout: timeout,
+		ReadTimeout:       timeout,
+		WriteTimeout:      timeout,
+		MaxHeaderBytes:    1 << 20,
+	}
+
+	log.Fatal(svr.ListenAndServe())
 }
